dc246c2bf29d231ef1bad68df04cece66cc3e894
Akshay-1024 <akshayshekkari24@gmail.com>
2020-05-01 15:00:47 +0530

    Just created plot1 RScript and png file

---
+++ plot1.R
@@ -0,0 +1,8 @@
+library(data.table)
+Dataset<-fread("household_power_consumption.txt")
+subSetData <- Dataset[Dataset$Date %in% c("1/2/2007","2/2/2007") ,]
+globalActivePower <- as.numeric(subSetData$Global_active_power)
+png("plot1.png", width=480, height=480)
+hist(globalActivePower, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+dev.off()
+
